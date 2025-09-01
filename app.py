8ddbbbc53217c36d8b276fbf8f3ef907b60418bb
Huy Le <huyle@Huys-MacBook-Air.local>
2025-09-01 19:55:39 +0700

    Cập nhật app.py với các tính năng mới

--- app.py
+++ app.py
@@ -238,4 +238,4 @@ def download():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=6002)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
